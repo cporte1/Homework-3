9bbc058001eb0b1a413db2b95e8686fb56d28523
Conrad porte <cporte1@student.gsu.edu>
2024-10-13 16:02:29 -0400

    added check for winning game, list of cards, the gameprovider,

--- hw3/lib/main.dart
+++ hw3/lib/main.dart
@@ -9,6 +9,31 @@ class Card {
   Card(this.front, this.back);
 }
 
+class GameProvider extends ChangeNotifier {
+  final List<Card> cards = [];
+
+  GameProvider() {
+    for (int = 0; i < 8; i++) {
+      cards.add(Card("assets/card_front.png", "assets/card_back.png"));
+    }
+  }
+
+void cardFlip(int card) {
+  cards[card].checkFaceUp = !cards[card].checkFaceUp;
+  if (checkGameWon()) {
+    print('You Won!');
+  }
+  notifyListener();
+}
+
+  bool checkGameWon() {
+    return cards
+        .where((card) => !card.checkFaceUp)
+        .isEmpty;
+  }
+}
+
+
 class CardMatchingGame extends StatelessWidget {
   @override
   Widget build(buildContext context) {
