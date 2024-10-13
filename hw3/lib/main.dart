46ebdd57d24b774f1552c1d96ad2414a04f05d27
Conrad porte <cporte1@student.gsu.edu>
2024-10-13 15:36:33 -0400

    intial import and card class creation

---
+++ hw3/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Card {
+  final String front;
+  final String back;
+  boot checkFaceUp = false;
+
+  Card(this.front, this.back);
+}
